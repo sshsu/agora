a9451df3bf1842248eaa1a93b2dc50fbad4f288c
yishen <yishen.wang@foxmail.com>
2020-02-11 23:54:14 -0800

    [add] first commit

---
+++ sample/OpenVideoCall/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./openVideoCall  --appId c7a4cc481d194849b8095b736e112b1e\
+                 --uid  202\
+                 --channel demoChannel \
+                 --dynamicKey 0 \
+                 --channelProfile 1 \
+                 --audioProfile 0 \
+                 --audioScenario 0 \
+                 --videoProfile 0 \
+                 --enableVideo 1 \
+                 --enableAudio 1 \
+                 --muteLocalVideo 0 \
+                 --muteLocalAudio 0 \
+                 --enableLocalVideo 1 \
+                 --enableWebSdkInteroperability 0 \
+                 --openVideoCall 0
