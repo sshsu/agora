5830f7db4d46c482fa6f2afeb99c4d660b217c98
yishen <wangys003@cmft.com>
2020-02-13 15:03:38 +0800

    [add] 添加将playback获取到的pcm写文件

---
+++ sample/OpenVideoCall/EngineModel/AudioFrameObserver.cpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "AudioFrameObserver.h"
+#include <unistd.h>
+
+bool AudioFrameObserver::onRecordAudioFrame(AudioFrame& audioFrame) 
+{
+	//printf("%s: samples=%d, channel=%d, fs=%d\n", __FUNCTION__, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
+return true;
+}
+
+bool AudioFrameObserver::onPlaybackAudioFrame(AudioFrame& audioFrame) 
+{
+	printf("%s: samples=%d, channel=%d, fs=%d\n", __FUNCTION__, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
+    file_t *fd = fopen("record.pcm", "ab+");
+    fputs(audioFrame.buffer, fd);
+    fclose(fd);
+	return true;
+}
+
+bool AudioFrameObserver::onMixedAudioFrame(AudioFrame& audioFrame)
+{
+	//printf("%s: samples=%d, channel=%d, fs=%d\n", __FUNCTION__, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
+	return true;
+}
+
+bool AudioFrameObserver::onPlaybackAudioFrameBeforeMixing(unsigned int uid, AudioFrame& audioFrame) 
+{
+//	printf("%s: uid=%u, samples=%d, channel=%d, fs=%d\n", __FUNCTION__, uid, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
+	return true;
+}
--- sample/OpenVideoCall/EngineModel/AudioFrameObserver.h
+++ sample/OpenVideoCall/EngineModel/AudioFrameObserver.h
@@ -6,24 +6,11 @@
 class AudioFrameObserver : public agora::media::IAudioFrameObserver
 {
 public:
-	virtual bool onRecordAudioFrame(AudioFrame& audioFrame) 
-	{
-		//printf("%s: samples=%d, channel=%d, fs=%d\n", __FUNCTION__, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
-		return true;
-	}
-	virtual bool onPlaybackAudioFrame(AudioFrame& audioFrame) 
-	{
-		printf("%s: samples=%d, channel=%d, fs=%d\n", __FUNCTION__, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
-		return true;
-	}
-	virtual bool onMixedAudioFrame(AudioFrame& audioFrame)
-	{
-		//printf("%s: samples=%d, channel=%d, fs=%d\n", __FUNCTION__, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
-		return true;
-	}
-	virtual bool onPlaybackAudioFrameBeforeMixing(unsigned int uid, AudioFrame& audioFrame) 
-	{
-	//	printf("%s: uid=%u, samples=%d, channel=%d, fs=%d\n", __FUNCTION__, uid, audioFrame.samples, audioFrame.channels, audioFrame.samplesPerSec);
-		return true;
-	}
+	virtual bool onRecordAudioFrame(AudioFrame& audioFrame) ;
+	virtual bool onPlaybackAudioFrame(AudioFrame& audioFrame) ;
+
+	virtual bool onMixedAudioFrame(AudioFrame& audioFrame);
+	
+	virtual bool onPlaybackAudioFrameBeforeMixing(unsigned int uid, AudioFrame& audioFrame) ;
+	
 };
